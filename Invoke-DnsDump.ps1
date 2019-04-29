22f823bb650b2ec1ba20de9d1bf65a1a26918812
3gstudent <15814408+3gstudent@users.noreply.github.com>
2019-04-28 18:08:39 -0700

    Update Invoke-DnsDump.ps1

--- Invoke-DnsDump.ps1
+++ Invoke-DnsDump.ps1
@@ -21,8 +21,8 @@
 ## I modified the search parameters to use in the new environment.(Eg.Server2012R2)
 ## Reference:https://github.com/mmessano/PowerShell/blob/master/dns-dump.ps1
 ## .EXAMPLE
-## PS C:\> Powershell -ep bypass -f dns-dump.ps1 -zone test.com
-## PS C:\> Powershell -ep bypass -f dns-dump.ps1 -zone test.com -csv
+## PS C:\> Powershell -ep bypass -f Invoke-DnsDump.ps1 -zone test.com
+## PS C:\> Powershell -ep bypass -f Invoke-DnsDump.ps1 -zone test.com -csv
 ##
 
 Param(
